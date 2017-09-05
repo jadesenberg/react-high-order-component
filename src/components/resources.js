d86925676fc4cf7922fa23cee0355afd53f31785
Jade Bautista <edaj.bautista@gmail.com>
2017-09-05 12:49:44 +0800

    Add resources component

---
+++ src/components/resources.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+    return (
+        <div>
+            Anime movie
+            <ul>
+                <li>A Silent Voice</li>
+                <li>Your Name</li>
+                <li>Your Lie in April</li>
+            </ul>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import App from './components/app';
+import Resources from './components/resources';
+import Header from './components/header';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -12,7 +14,13 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
-      <App />
+        <div>
+          <Header />
+          <Switch>
+            <Route path="/resources" component={Resources} />
+            <Route path="/" component={App} />
+          </Switch>    
+        </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
